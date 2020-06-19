662d7c81332abb2a4801e70859a846d91fd5c5cd
bhargav <ptlbhargav93@gmai.com>
2020-06-19 14:25:03 +0530

    fixed billing and shipping name

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,5 +6,5 @@ class Customer < ActiveRecord::Base
     belongs_to :creator, class_name: "User", foreign_key: "creator_id"
     belongs_to :updater, class_name: "User", foreign_key: "updater_id"		
 
-	validates :name, :presence => true
+	validates :b_name, :presence => true
 end
