6e7de819d6be863b49794f526da904b8197ff4b2
bhargav <ptlbhargav93@gmail.com>
2020-08-19 21:24:20 +0530

    fixed changes

--- app/assets/javascripts/website/acommon.js
+++ app/assets/javascripts/website/acommon.js
@@ -1,5 +1,85 @@
 $(document).ready(function(){
 
+  var substringMatcher = function(strs) {
+    return function findMatches(q, cb) {
+      var matches, substringRegex;
+
+      // an array that will be populated with substring matches
+      matches = [];
+
+      // regex used to determine if a string contains the substring `q`
+      substrRegex = new RegExp(q, 'i');
+
+      // iterate through the pool of strings and for any string that
+      // contains the substring `q`, add it to the `matches` array
+      $.each(strs, function(i, str) {
+        if (substrRegex.test(str)) {
+          matches.push(str);
+        }
+      });
+
+      cb(matches);
+    };
+  };
+
+
+  var states = [ "Andhra Pradesh",
+                "Arunachal Pradesh",
+                "Assam",
+                "Bihar",
+                "Chhattisgarh",
+                "Goa",
+                "Gujarat",
+                "Haryana",
+                "Himachal Pradesh",
+                "Jammu and Kashmir",
+                "Jharkhand",
+                "Karnataka",
+                "Kerala",
+                "Madhya Pradesh",
+                "Maharashtra",
+                "Manipur",
+                "Meghalaya",
+                "Mizoram",
+                "Nagaland",
+                "Odisha",
+                "Punjab",
+                "Rajasthan",
+                "Sikkim",
+                "Tamil Nadu",
+                "Telangana",
+                "Tripura",
+                "Uttarakhand",
+                "Uttar Pradesh",
+                "West Bengal",
+                "Andaman and Nicobar Islands",
+                "Chandigarh",
+                "Dadra and Nagar Haveli",
+                "Daman and Diu",
+                "Delhi",
+                "Lakshadweep",
+                "Puducherry"];
+
+  $('#the-basics .typeahead').typeahead({
+    input: 'Typeahead-input',
+    hint: true,
+    highlight: true,
+    minLength: 1
+  },
+  {
+    name: 'states',
+    source: substringMatcher(states)
+  });
+
+  if ($(".customer-association-fields").length > 0){
+  }
+  else {
+    if ($(".customer-association-fields").find('add_fields')){
+      $("#customer_bill_products").find(".add_fields").click();
+      $(".customer-association-fields").find(".add_fields").remove();  
+    }
+  }
+
   common_events();
 
   $(document).on("change", ".UploadedMedia", function(){
