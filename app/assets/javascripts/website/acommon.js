ea69698f59ab28c14ab67bf65fe899ab68c6e288
bhargav <ptlbhargav93@gmail.com>
2020-08-31 14:12:28 +0530

    revert typahed js

--- app/assets/javascripts/website/acommon.js
+++ app/assets/javascripts/website/acommon.js
@@ -1,28 +1,5 @@
 $(document).ready(function(){
 
-  var substringMatcher = function(strs) {
-    return function findMatches(q, cb) {
-      var matches, substringRegex;
-
-      // an array that will be populated with substring matches
-      matches = [];
-
-      // regex used to determine if a string contains the substring `q`
-      substrRegex = new RegExp(q, 'i');
-
-      // iterate through the pool of strings and for any string that
-      // contains the substring `q`, add it to the `matches` array
-      $.each(strs, function(i, str) {
-        if (substrRegex.test(str)) {
-          matches.push(str);
-        }
-      });
-
-      cb(matches);
-    };
-  };
-
-
   var states = [ "ANDHRA PRADESH",
                 "ARUNACHAL PRADESH",
                 "ASSAM",
@@ -60,16 +37,6 @@ $(document).ready(function(){
                 "LAKSHADWEEP",
                 "PUDUCHERRY"];;
 
-  $('#the-basics .typeahead').typeahead({
-    input: 'Typeahead-input',
-    hint: true,
-    highlight: true,
-    minLength: 1
-  },
-  {
-    name: 'states',
-    source: substringMatcher(states)
-  });
 
   if ($(".customer-association-fields").length > 0){
   }
